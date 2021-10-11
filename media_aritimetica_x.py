d14859ea260d467cc0d80f564b801763d8000bc8
Wellington  Farina <59116088+Wellington-Farina-Ferraz@users.noreply.github.com>
2021-10-11 08:53:11 +0000

    projeto_python

---
+++ media_aritimetica_x.py
@@ -0,0 +1,17 @@
+print("Digite zero para finalizar o Programa .")
+
+soma = 0  #variaveu par guarda a somas
+cont = 0  # variavel par contar quanta vez passou no laco
+result = 0  # variavel para guarda o resultado
+valor = 1  # variavel para guarda o valor digitado pelo usuario
+
+while valor != 0:
+    cont = cont + 1
+    valor = int(input("digite o valor: "))
+    soma = soma + valor  # soma os valors digitado // Digitado 0 ele sai do laco
+
+cont = cont - 1  # retira 1 para deixar a divisao exata
+result = soma / cont  # calcula o resultado
+
+print("sua conta é: ", soma, " / ", cont)
+print("A media aritimetica é: ", result)  #imprime o resultado
